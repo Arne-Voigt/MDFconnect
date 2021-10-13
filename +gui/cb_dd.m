2236f690efc4ed413c388517a42a766db458c959
Arne-Voigt <kiwiofsuburbia@gmail.com>
2021-10-13 22:30:18 +0200

    gui update -> at  dipslay mdfObj change the tree in correctly updated

--- +gui/cb_dd.m
+++ +gui/cb_dd.m
@@ -9,10 +9,17 @@ if isempty(ObjectH.Value); return; end;
 evalin('base', 'treeContainer.delete');
 
 objName = ObjectH.String{ObjectH.Value};
-objToDraw = evalin('base', objName);
+%objToDraw = evalin('base', objName);
 hc_main = evalin('base', 'hc_main');
-[mtree, treeContainer] = uitree('v0', 'root', objToDraw.getTreeNode(),  'Parent', hc_main); % Parent is ignored
-assignin('base', 'treeContainer',  treeContainer);
+
+%objToDraw = evalin('base', mdfObjects{1});
+strTreeInfo = sprintf('%s.getTreeNode()', objName);
+uiTreeNode = evalin('base', strTreeInfo);
+
+%[mtree, treeContainer] = uitree('v0', 'root', objToDraw.getTreeNode(),      'Parent', hc_main); % Parent is ignored
+[mtree, treeContainer] = uitree('v0', 'root', uiTreeNode,  'Parent', hc_main); % Parent is ignored
+
+
 
 clear objToDraw     
 jtree = mtree.getTree;
@@ -27,6 +34,8 @@ set(treeContainer, 'Parent', hc_main)
 
 mtree.expand(mtree.Root.getChildAt(1))
 
+assignin('base', 'treeContainer',  treeContainer);
+assignin('base', 'jtree',  jtree);
 drawnow
 
 end
--- +gui/cb_mbp.m
+++ +gui/cb_mbp.m
@@ -11,14 +11,13 @@ import javax.swing.tree.*;
 %       fprintf('shift: %i;\n', eventData.isControlDown);
 %       fprintf('alt:   %i;\n', eventData.isAltDown);
       treePath = jtree.getPathForLocation(clickX, clickY);
+      
       if isempty(treePath); return; end;
       if eventData.getClickCount() ~= 1; return; end;
       if eventData.isControlDown ~= 1; return; end;
-          nr=eventData.getClickCount();
-          %disp([num2str(x) ': click count:' num2str(nr)]);
-          % check if the checkbox was clicked
-          node = treePath.getLastPathComponent;
-          nodeValue = node.getValue;
-          ans = evalin('base', nodeValue)
-          assignin('base', 'ans', ans);
+      
+      node = treePath.getLastPathComponent;
+      nodeValue = node.getValue;
+      ans = evalin('base', nodeValue)
+      assignin('base', 'ans', ans);
 end % function mousePressedCallback
