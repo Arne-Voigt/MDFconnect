9539e99edbd08ce677652a2be9761c583e25ab91
Arne-Voigt <kiwiofsuburbia@gmail.com>
2021-11-14 18:35:46 +0100

    1ST

--- simpleTest.m
+++ simpleTest.m
@@ -1,9 +1,10 @@
 classdef simpleTest < matlab.unittest.TestCase
 
-    methods
+    methods (Test)
         % list of all tests to run (this should be out-sourced)
         function isEqualTest(testCase)
                 testCase.assertEqual('11','11');
+                disp('tested 11')
         end
     end
     
